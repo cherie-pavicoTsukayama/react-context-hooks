e33110dadb7bc95bb749dc8f86276280e2c946a0
Cherie <cherietsukayama@gmail.com>
2020-08-13 00:26:47 +0000

    added the BookList component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import BookList from './components/Booklist';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar />
+      <BookList />
     </div>
   );
 }
