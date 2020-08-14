35a9a6d116f6c6c717a957ba896343fef8f58481
Cherie <cherietsukayama@gmail.com>
2020-08-14 16:49:24 +0000

    refactored the class component to a function and used the hook useContext to pass the context to react elemnts

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemedContext';
 
-export default class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className="book-list"
-        style={{ background: theme.bg, color: theme.syntax }}>
-        <ul>
-          <li style={{ background: theme.ui }}>The Way Of Kings</li>
-          <li style={{ background: theme.ui }}>The Name of the Wind</li>
-          <li style={{ background: theme.ui }}>The Final Empire</li>
-        </ul>
-      </div>
-    )
-  }
+// export default class BookList extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+//     return (
+//       <div
+//         className="book-list"
+//         style={{ background: theme.bg, color: theme.syntax }}>
+//         <ul>
+//           <li style={{ background: theme.ui }}>The Way Of Kings</li>
+//           <li style={{ background: theme.ui }}>The Name of the Wind</li>
+//           <li style={{ background: theme.ui }}>The Final Empire</li>
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+ const BookList = () => {
+  const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}>
+      <ul>
+        <li style={{ background: theme.ui }}>The Way Kings</li>
+        <li style={{ background: theme.ui }}>The Name of the Wind</li>
+        <li style={{ background: theme.ui }}>The Final Empire</li>
+      </ul>
+    </div>
+  );
 }
+
+export default BookList;
